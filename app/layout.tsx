1b1a497d5029afaff0ac77c6c3a566be60057cdd
Gavin Hall <gavin@magnet.co>
2024-02-04 01:58:45 -0500

    edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,6 @@
 import './globals.css'
-// import { Inter } from 'next/font/google'
-import { ThemeProvider } from '@/components/theme-provider'
 import { Analytics } from '@/components/analytics'
 import Header from '@/components/header' // Import the Header component
-
-// const inter = Inter({ subsets: ['latin'] })
-// ${inter.className} is placed on the body className when inter is in use
 
 export const metadata = {
   title: 'Oyster',
@@ -19,14 +14,12 @@ interface RootLayoutProps {
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en" className="text-[19px]">
-      <body className={`min-h-screen bg-[#0F0F0F] font-primary antialiased `}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="mx-auto max-w-screen-xl px-4 py-10">
-            <Header /> {/* Use the Header component */}
-            <main>{children}</main>
-          </div>
-          <Analytics />
-        </ThemeProvider>
+      <body className={`min-h-screen bg-[#0F0F0F] text-[#939393] font-primary antialiased `}>
+        <div className="mx-auto max-w-screen-xl px-4 py-10">
+          <Header />
+          <main>{children}</main>
+        </div>
+        <Analytics />
       </body>
     </html>
   )
